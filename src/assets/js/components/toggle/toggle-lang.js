0a81ec16883a9411b9e45185690e84765b672ac3
wotkad <wotkad@gmail.com>
2023-08-30 22:20:54 +0300

    update

--- src/assets/js/components/toggle/toggle-lang.js
+++ src/assets/js/components/toggle/toggle-lang.js
@@ -4,25 +4,25 @@ function toggleLang() {
   let items = $('.header__languages a');
   button.on('click', function() {
     $(this).addClass('active');
-    menu.fadeIn();
+    menu.fadeIn(300);
   });
   Array.from(items).forEach(function(item) {
     $(item).on('click', function() {
       items.removeClass('current');
       $(this).addClass('current');
       button.removeClass('active');
-      menu.fadeOut();
+      menu.fadeOut(300);
     });
   });
   $(document).mouseup( function(e) {
     if (!menu.is(e.target)) {
-      menu.fadeOut();
+      menu.fadeOut(300);
       button.removeClass('active');
     }
   });
   document.addEventListener('keydown', function(e) {
     if( e.key == 'Escape' ){
-      menu.fadeOut();
+      menu.fadeOut(300);
     }
   });
 }
