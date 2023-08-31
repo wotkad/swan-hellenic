f41fab87ea390c3561fa5055b15eb9679e992140
wotkad <wotkad@gmail.com>
2023-09-01 00:32:05 +0300

    add footer

--- src/assets/js/components/scroll/scroll-header.js
+++ src/assets/js/components/scroll/scroll-header.js
@@ -6,6 +6,7 @@ function scrollHeader() {
   let items = $('.header__menu > .header__item > .header__link')
   let container = $('.wrapper');
   let logo = $('.header__bottom .header__logo__inner');
+  let main = $('main');
   gsap.to(logo, 0, {x: -(container.width() - menu.width()) / 2});
   $(window).on('scroll', function() {
     $('.header__languages').fadeOut(300);
@@ -14,10 +15,12 @@ function scrollHeader() {
     if (window.pageYOffset > 0) {
       header.addClass('scrolled');
       logo.addClass('active');
+      main.addClass('scrolled');
       gsap.to(items, 0.3, {x: (container.width() - menu.width()) / 2});
     } else {
       header.removeClass('scrolled');
       logo.removeClass('active');
+      main.removeClass('scrolled');
       gsap.to(items, 0.3, {x: 0});
     }
   });
