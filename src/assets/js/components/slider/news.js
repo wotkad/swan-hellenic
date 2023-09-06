9bd05a0646d537e8f5c6937eabb017dba2bcda09
wotkad <wotkad@gmail.com>
2023-09-07 02:13:58 +0300

    fix

--- src/assets/js/components/slider/news.js
+++ src/assets/js/components/slider/news.js
@@ -1,21 +1,22 @@
 import Splide from '@splidejs/splide';
 
 function newsSlider() {
-  new Splide( '.news-block', {
-    // type: 'loop',
-    gap: 24,
-    arrows: false,
-    pagination: false,
-    perPage: 1,
-    padding: { right: 80},
-    drag: true,
-    perMove: 1,
-    mediaQuery: 'min',
-    breakpoints: {
-      768: {
-        destroy: true,
-      },
-    }
-  }).mount();
+  if ($('.news-block').length !== 0) {
+    new Splide('.news-block', {
+      gap: 24,
+      arrows: false,
+      pagination: false,
+      perPage: 1,
+      padding: { right: 80 },
+      drag: true,
+      perMove: 1,
+      mediaQuery: 'min',
+      breakpoints: {
+        768: {
+          destroy: true,
+        },
+      }
+    }).mount();
+  }
 }
 newsSlider();
