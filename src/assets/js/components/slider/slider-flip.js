a4f6c64608739e42332bc9ab579207a1b41c0059
wotkad <wotkad@gmail.com>
2023-09-08 01:02:57 +0300

    update flip slider

--- src/assets/js/components/slider/slider-flip.js
+++ src/assets/js/components/slider/slider-flip.js
@@ -1,30 +1,34 @@
-// import Splide from '@splidejs/splide';
+import Splide from '@splidejs/splide';
 
-// function initializeSliders() {
-//     const sliderContainers = document.querySelectorAll('[data-slider]');
+function initializeSliders() {
+    const sliderContainers = document.querySelectorAll('[data-slider]');
+    let i = 0;
+    sliderContainers.forEach((container, index) => {
 
-//     sliderContainers.forEach((container, index) => {
-//         const mainSelector = `.${container.getAttribute('data-slider')}-images`;
-//         const thumbnailsSelector = `.${container.getAttribute('data-slider')}-list`;
+        const mainSelector = `.${container.getAttribute('data-slider')}${i}__images`;
+        const thumbnailsSelector = `.${container.getAttribute('data-slider')}${i}__list`;
 
-//         let main = new Splide(mainSelector, {
-//             type: 'fade',
-//             arrows: false,
-//             pagination: false,
-//             drag: false,
-//         });
+        console.log(mainSelector)
 
-//         let thumbnails = new Splide(thumbnailsSelector, {
-//             arrows: false,
-//             isNavigation: true,
-//             pagination: false,
-//             drag: false,
-//             perPage: 'auto',
-//         });
+        let main = new Splide(mainSelector, {
+            type: 'fade',
+            arrows: false,
+            pagination: false,
+            drag: false,
+        });
 
-//         main.sync(thumbnails);
-//         main.mount();
-//         thumbnails.mount();
-//     });
-// }
-// initializeSliders();
+        let thumbnails = new Splide(thumbnailsSelector, {
+            arrows: false,
+            isNavigation: true,
+            pagination: false,
+            drag: false,
+            perPage: 'auto',
+        });
+
+        main.sync(thumbnails);
+        main.mount();
+        thumbnails.mount();
+        i++;
+    });
+}
+initializeSliders();
