141b3750767e3c470fc5865e8246179843e8c9d4
wotkad <wotkad@gmail.com>
2023-09-08 13:45:14 +0300

    update

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,7 +11,6 @@ import "./base/common/preloader";
 import "./base/common/smooth-scroll";
 import "./base/common/scroll-reveal";
 
-import "./base/checks/check-internal-links";
 import "./base/checks/check-target";
 import "./base/common/get-current-year";
 import "./base/common/get-header-height";
