d62069417e5a596cf27de783499b112934ab2c3e
wotkad <wotkad@gmail.com>
2023-09-08 03:44:44 +0300

    update

--- src/assets/js/components/slider/slider-flip.js
+++ src/assets/js/components/slider/slider-flip.js
@@ -1,34 +1,49 @@
 import Splide from '@splidejs/splide';
 
 function initializeSliders() {
-    const sliderContainers = document.querySelectorAll('[data-slider]');
-    let i = 0;
-    sliderContainers.forEach((container, index) => {
+  const sliderContainers = document.querySelectorAll('[data-slider]');
+  let i = 0;
+  sliderContainers.forEach((container, index) => {
 
-        const mainSelector = `.${container.getAttribute('data-slider')}${i}__images`;
-        const thumbnailsSelector = `.${container.getAttribute('data-slider')}${i}__list`;
+    const mainSelector = `.${container.getAttribute('data-slider')}${i}__images`;
+    const thumbnailsSelector = `.${container.getAttribute('data-slider')}${i}__list`;
 
-        console.log(mainSelector)
-
-        let main = new Splide(mainSelector, {
-            type: 'fade',
-            arrows: false,
-            pagination: false,
-            drag: false,
-        });
-
-        let thumbnails = new Splide(thumbnailsSelector, {
-            arrows: false,
-            isNavigation: true,
-            pagination: false,
-            drag: false,
-            perPage: 'auto',
-        });
-
-        main.sync(thumbnails);
-        main.mount();
-        thumbnails.mount();
-        i++;
+    let main = new Splide(mainSelector, {
+      arrows: false,
+      pagination: false,
+      mediaQuery: 'min',
+      type: 'fade',
     });
+
+    let thumbnails = new Splide(thumbnailsSelector, {
+      arrows: true,
+      isNavigation: true,
+      pagination: false,
+      mediaQuery: 'min',
+      drag: true,
+      perPage: 1,
+      classes: {
+        arrow : 'flip-slider__arrow'
+      },
+      breakpoints: {
+        1240: {
+          arrows: false,
+          drag: false,
+          perPage: 'auto',
+        },
+      }
+    });
+
+    main.sync(thumbnails);
+    main.mount();
+    thumbnails.mount();
+    i++;
+
+  });
+
 }
 initializeSliders();
+
+$(window).on('resize', function() {
+  initializeSliders();
+});
