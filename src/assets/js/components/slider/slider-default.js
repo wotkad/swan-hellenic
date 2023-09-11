be1c461a3f96dcda243c13359c8f1f645f99443e
wotkad <wotkad@gmail.com>
2023-09-11 19:52:39 +0300

    fixes

--- src/assets/js/components/slider/slider-default.js
+++ src/assets/js/components/slider/slider-default.js
@@ -1,41 +1,43 @@
 import Splide from '@splidejs/splide';
 
 function sliderDefault() {
-  const sliderContainers = document.querySelectorAll('.default-slider');
-  let i = 0;
-  sliderContainers.forEach((container, index) => {
-    
-    const mainSelector = `.${container.getAttribute('data-slider')}${i}__track`;
+  if ($('.default-slider').length !== 0) {
+    const sliderContainers = document.querySelectorAll('.default-slider');
+    let i = 0;
+    sliderContainers.forEach((container, index) => {
+      
+      const mainSelector = `.${container.getAttribute('data-slider')}${i}__track`;
 
-    let main = new Splide(mainSelector, {
-      mediaQuery: 'min',
-      lazyLoad: 'nearby',
-      gap: 20,
-      drag: true,
-      pagination: false,
-      arrows: false,
-      perPage: 1,
-      padding: { right: 100 },
-      speed: 800,
-      breakpoints: {
-        1240: {
-          gap: 40,
-          perPage: 3,
-          padding: { right: 0 },
-          arrows: true,
-        },
-        768: {
-          gap: 40,
-          perPage: 2,
-          padding: { right: 120 },
-        },
-      }
+      let main = new Splide(mainSelector, {
+        mediaQuery: 'min',
+        lazyLoad: 'nearby',
+        gap: 20,
+        drag: true,
+        pagination: false,
+        arrows: false,
+        perPage: 1,
+        padding: { right: 100 },
+        speed: 800,
+        breakpoints: {
+          1240: {
+            gap: 40,
+            perPage: 3,
+            padding: { right: 0 },
+            arrows: true,
+          },
+          768: {
+            gap: 40,
+            perPage: 2,
+            padding: { right: 120 },
+          },
+        }
+      });
+
+      main.mount();
+      i++;
+
     });
-
-    main.mount();
-    i++;
-
-  });
+  }
 
 }
 sliderDefault();
--- src/assets/js/components/slider/slider-flip.js
+++ src/assets/js/components/slider/slider-flip.js
@@ -1,52 +1,53 @@
 import Splide from '@splidejs/splide';
 
 function sliderFlip() {
-  const sliderContainers = document.querySelectorAll('.flip-slider');
-  let i = 0;
-  sliderContainers.forEach((container, index) => {
+  if ($('.flip-slider').length !== 0) {
+    const sliderContainers = document.querySelectorAll('.flip-slider');
+    let i = 0;
+    sliderContainers.forEach((container, index) => {
 
-    const mainSelector = `.${container.getAttribute('data-slider')}${i}__images`;
-    const thumbnailsSelector = `.${container.getAttribute('data-slider')}${i}__list`;
+      const mainSelector = `.${container.getAttribute('data-slider')}${i}__images`;
+      const thumbnailsSelector = `.${container.getAttribute('data-slider')}${i}__list`;
 
-    let main = new Splide(mainSelector, {
-      arrows: false,
-      pagination: false,
-      mediaQuery: 'min',
-      lazyLoad: 'nearby',
-      drag: false,
-      gap: 20,
-      speed: 800,
-      breakpoints: {
-        1240: {
-          gap: 40
-        },
-      }
+      let main = new Splide(mainSelector, {
+        arrows: false,
+        pagination: false,
+        mediaQuery: 'min',
+        lazyLoad: 'nearby',
+        drag: false,
+        gap: 20,
+        speed: 800,
+        breakpoints: {
+          1240: {
+            gap: 40
+          },
+        }
+      });
+
+      let thumbnails = new Splide(thumbnailsSelector, {
+        arrows: true,
+        isNavigation: true,
+        pagination: false,
+        mediaQuery: 'min',
+        drag: true,
+        perPage: 1,
+        speed: 800,
+        breakpoints: {
+          1240: {
+            arrows: false,
+            drag: false,
+            perPage: 'auto',
+          },
+        }
+      });
+
+      main.sync(thumbnails);
+      main.mount();
+      thumbnails.mount();
+      i++;
+
     });
-
-    let thumbnails = new Splide(thumbnailsSelector, {
-      arrows: true,
-      isNavigation: true,
-      pagination: false,
-      mediaQuery: 'min',
-      drag: true,
-      perPage: 1,
-      speed: 800,
-      breakpoints: {
-        1240: {
-          arrows: false,
-          drag: false,
-          perPage: 'auto',
-        },
-      }
-    });
-
-    main.sync(thumbnails);
-    main.mount();
-    thumbnails.mount();
-    i++;
-
-  });
-
+  }
 }
 sliderFlip();
 
