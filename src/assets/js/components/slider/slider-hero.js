360a942719f81e1530c558cc625f99dec6b40655
wotkad <wotkad@gmail.com>
2023-09-11 13:58:03 +0300

    update hero

--- src/assets/js/components/slider/slider-hero.js
+++ src/assets/js/components/slider/slider-hero.js
@@ -5,10 +5,10 @@ function sliderHero() {
     let images = new Splide('.hero__images', {
       type: 'fade',
       rewind: true,
+      pagination: false,
       arrows: false,
       perPage: 1,
       lazyLoad: 'nearby',
-      pagination: false,
     });
     let content = new Splide('.hero__content', {
       type: 'fade',
@@ -16,7 +16,7 @@ function sliderHero() {
       arrows: true,
       perPage: 1,
       drag: false,
-      pagination: false,
+      pagination: true,
     });
     images.sync(content);
     images.mount();
