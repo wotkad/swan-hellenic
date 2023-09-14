a7f623c37eb530cd572cf95a1f83c3fdd1b4f849
tori-vlsv <victoriaavlasova@gmail.com>
2023-09-14 18:20:53 +0300

    update cruise page

---
+++ src/assets/js/components/slider/slider-suite.js
@@ -0,0 +1,48 @@
+import Splide from '@splidejs/splide';
+
+function sliderDefault() {
+  if ($('.card-suite__slider').length !== 0) {
+    const sliderContainers = document.querySelectorAll('.card-suite__slider');
+    let i = 0;
+    sliderContainers.forEach((container, index) => {
+
+      const mainSelector = `.${container.getAttribute('data-slider')}${i}__track`;
+
+      let main = new Splide(mainSelector, {
+        mediaQuery: 'min',
+        lazyLoad: 'nearby',
+        gap: 20,
+        drag: true,
+        pagination: false,
+        arrows: false,
+        perPage: 1,
+        padding: { right: 100 },
+        speed: 800,
+        perMove: 1,
+        breakpoints: {
+          1240: {
+            gap: 40,
+            perPage: 3,
+            padding: { right: 0 },
+            arrows: true,
+          },
+          768: {
+            gap: 40,
+            perPage: 2,
+            padding: { right: 120 },
+          },
+        }
+      });
+
+      main.mount();
+      i++;
+
+    });
+  }
+
+}
+sliderDefault();
+
+$(window).on('resize', function() {
+  sliderDefault();
+});
