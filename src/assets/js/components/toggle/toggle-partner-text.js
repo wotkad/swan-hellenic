86f1317413f43ddfd94aa42f534f8b11a72dbd45
wotkad <wotkad@gmail.com>
2024-01-10 15:43:43 +0300

    update

--- src/assets/js/components/toggle/toggle-partner-text.js
+++ src/assets/js/components/toggle/toggle-partner-text.js
@@ -1,8 +1,12 @@
 function togglePartnerText() {
   let button = $('.partner-block__toggle');
   button.on('click', function() {
-    $(this).prev().addClass('active');
-    $(this).hide();
+    $(this).prev().toggleClass('active');
+    if ($(this).hasClass('active')) {
+      $(this).text('Show more');
+    } else {
+      $(this).text('Show less');
+    }
   });
 }
 togglePartnerText();
